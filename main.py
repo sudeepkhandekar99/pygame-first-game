5a0d9b777927b8fd8c31d644b1a5cfa67d419e6c
sudeepkhandekar99 <sudeepkhandekar99@gmail.com>
2021-05-15 23:32:43 +0530

    player initialization and movements

--- main.py
+++ main.py
@@ -6,9 +6,36 @@ pygame.init()
 # Create the screen
 screen = pygame.display.set_mode((800, 600))
 
+# Title and Icon
+pygame.display.set_caption("space boi")
+icon = pygame.image.load('icon.img')
+pygame.display.set_icon(icon)
+
+# Player
+playerImg = pygame.image.load('spirit.png')
+playerX = 370
+playerY = 480
+
+def player(x, y):
+    screen.blit(playerImg, (x, y))
+
 # Game Loop
 running = True
 while running:
+    screen.fill((0, 0, 0))
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+    
+    # Movements
+    if event.type == pygame.KEYDOWN:
+        if event.key == pygame.K_LEFT:
+            print("Lpressed")
+        if event.key == pygame.K_RIGHT:
+            print("Rpressed")
+    if event.type == pygame.KEYUP:
+        if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
+            #print("Dpressed")
+    
+    player(playerX, playerY)
+    pygame.display.update()
