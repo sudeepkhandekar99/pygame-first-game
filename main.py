76f58256d6b46ed303a8d0548106540ca607248e
sudeepkhandekar99 <sudeepkhandekar99@gmail.com>
2021-05-16 11:39:59 +0530

    bullet mechanism

--- main.py
+++ main.py
@@ -23,10 +23,18 @@ playerX_change = 0
 
 # Enemy
 enemyImg = pygame.image.load('alien.png')
-enemyX = random.randint(0,800)
-enemyY = random.randint(50,150)
+enemyX = random.randint(70, 700)
+enemyY = random.randint(50, 150)
 enemyX_change = 0.15
-enemyY_change = 40
+enemyY_change = 10
+
+# Bullet
+bulletImg = pygame.image.load('bullet.png')
+bulletX = 0
+bulletY = 480
+bulletX_change = 0
+bulletY_change = 0.5
+bullet_state = "ready"  # Bullet ready and fire states
 
 
 def player(x, y):
@@ -37,7 +45,12 @@ def enemy(x, y):
     screen.blit(enemyImg, (x, y))
 
 
-# Game Loop
+def fire_bullet(x, y):
+    global bullet_state
+    bullet_state = "fire"
+    screen.blit(bulletImg, (x+16, y+10))
+
+    # Game Loop
 running = True
 while running:
     # Background
@@ -53,9 +66,12 @@ while running:
                 playerX_change = -0.3
             if event.key == pygame.K_RIGHT:
                 playerX_change = 0.3
+            if event.key == pygame.K_SPACE:
+                fire_bullet(playerX, bulletY)
         if event.type == pygame.KEYUP:
             if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
                 playerX_change = 0
+            
 
     # Checking for boundaries
     playerX += playerX_change
@@ -63,7 +79,7 @@ while running:
         playerX = 0
     elif playerX > 736:
         playerX = 736
-    
+
     enemyX += enemyX_change
     if enemyX <= 0:
         enemyX_change = 0.15
@@ -72,7 +88,11 @@ while running:
         enemyX_change = -0.15
         enemyY += enemyY_change
 
-        
+    # Bullet movement
+    if bullet_state is "fire":
+        fire_bullet(playerX, bulletY)
+        bulletY -= bulletY_change
+
     player(playerX, playerY)
     enemy(enemyX, enemyY)
     pygame.display.update()
