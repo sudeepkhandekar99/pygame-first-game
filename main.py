8a2e53fc456af713910c48c1932087193cab6f6e
sudeepkhandekar99 <sudeepkhandekar99@gmail.com>
2021-05-16 10:31:22 +0530

    enemy movements completed

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
 import pygame
+import random
 
 # Intialize the pygame
 pygame.init()
 
 # Create the screen
 screen = pygame.display.set_mode((800, 600))
+
+# Background
+background = pygame.image.load('background.jpg')
 
 # Title and Icon
 pygame.display.set_caption("space boi")
@@ -19,9 +23,10 @@ playerX_change = 0
 
 # Enemy
 enemyImg = pygame.image.load('alien.png')
-enemyX = 370
-enemyY = 50
-enemyX_change = 0
+enemyX = random.randint(0,800)
+enemyY = random.randint(50,150)
+enemyX_change = 0.15
+enemyY_change = 40
 
 
 def player(x, y):
@@ -35,7 +40,8 @@ def enemy(x, y):
 # Game Loop
 running = True
 while running:
-    screen.fill((150, 150, 150))
+    # Background
+    screen.blit(background, (0, 0))
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -51,11 +57,21 @@ while running:
             if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
                 playerX_change = 0
 
+    # Checking for boundaries
     playerX += playerX_change
     if playerX < 0:
         playerX = 0
-    elif playerX > 768:
-        playerX = 768
+    elif playerX > 736:
+        playerX = 736
+    
+    enemyX += enemyX_change
+    if enemyX <= 0:
+        enemyX_change = 0.15
+        enemyY += enemyY_change
+    elif enemyX >= 736:
+        enemyX_change = -0.15
+        enemyY += enemyY_change
+
         
     player(playerX, playerY)
     enemy(enemyX, enemyY)
